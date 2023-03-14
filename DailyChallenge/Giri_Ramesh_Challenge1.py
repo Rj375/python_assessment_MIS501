1ccf1f3a57352c1fe1bab2653a7fd418b2d692b8
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-03-15 00:03:43 +1030

    added a new Folder called DailyChallenge

---
+++ DailyChallenge/Giri_Ramesh_Challenge1.py
@@ -0,0 +1,9 @@
+#Create a program to ask the user to enter marks for assessment 1, 2 and 3 and print the average of all.
+assessment1 = float(input("Enter result oF assessment 1:- "))
+assessment2 = float(input("Enter result oF assessment 2:- "))
+assessment3 = float(input("Enter result oF assessment 3:- "))
+
+#this adds the marks oF assessment 1,2 and 3
+total_avg_marks = assessment1 + assessment2 + assessment3 
+
+print("The Total Average Marks oF assessment is:- ", total_avg_marks/3) 
