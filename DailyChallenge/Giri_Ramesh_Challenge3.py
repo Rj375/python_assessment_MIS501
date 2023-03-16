4f732dc72510ae3bb3b8945d16726943733158ea
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-03-16 13:35:52 +1030

    made small change in new FIle

--- DailyChallenge/Giri_Ramesh_Challenge3.py
+++ DailyChallenge/Giri_Ramesh_Challenge3.py
@@ -3,7 +3,7 @@ num1 = eval(input("Enter num1:- "))
 num2 = eval(input("Enter num2:- "))
 num3 = eval(input("Enter num3:- "))
 
-if num1 > num2 and num1 > num2:
+if num1 > num2 and num1 > num3:
     print("The Maximum number is:-", num1)
 elif num2 > num1 and num2 > num3:
     print("The Maximum number is:-", num2)
