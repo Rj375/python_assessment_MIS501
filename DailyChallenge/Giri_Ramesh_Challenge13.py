c0defa630ac9dd45ba82e9b672bf34c326c7d701
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-03-31 19:33:09 +1030

    added new way oF doing DailyChallenge number 13

--- DailyChallenge/Giri_Ramesh_Challenge13.py
+++ DailyChallenge/Giri_Ramesh_Challenge13.py
@@ -23,5 +23,34 @@ while True:
             else:
                 print("Welcome", dummyUserName)
 
+# or
 
+# Create a program that ask the user to reset the password if the entered 
+# password not matched for 3 attempts.
+# Note : in case of user name not matching it must advise the user to sign up.
 
+username = ['username','username1','username2']
+password = ['secretpassword','secretpassword1', 'secretpassword2']
+
+#Attempt counter
+attempts = 0 
+reset_password = False
+while attempts <3:
+    input_username = input('Please enter your username: ')
+    input_password = input('Please enter your password: ')
+  
+    if input_username in username:
+        index = username.index(input_username)
+        if input_password == password[index]:
+            print('Sucessful login!')
+            break
+        else:
+            attempts += 1
+            if attempts < 3:
+                print('You have entered incorrect password. Please try again.')
+            if attempts == 3:
+                reset_password: True
+                print('You need to reset your password.')
+    else:
+        print('You need to register first!')
+        break
