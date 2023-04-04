8701aec1b82c6e7ec5ecf808398eeac597e3fea3
Ramesh Giri <rameshgiri@Rameshs-MacBook.local>
2023-04-05 01:33:55 +0930

    made small changes in new FIle

--- DailyChallenge/Giri_Ramesh_Challenge15.py
+++ DailyChallenge/Giri_Ramesh_Challenge15.py
@@ -12,19 +12,19 @@
 
 counter = 3
 
-while counter > 0:
-    userPassword = input("Enter your password: ")
+while counter > 0: #loop executes untill the counter is greater than 0.
+    userPassword = input("Enter the password:- ")
     
-    if userPassword.isdigit():
+    if userPassword.isdigit(): #checks whether the password contains all the numbers or not.
         print("The password must include alphabets as well.")
-    elif userPassword.isalpha():
+    elif userPassword.isalpha(): #checks whether the password contains all the alphabets or not.
         print("The password must include digits as well.")
-    elif len(userPassword) < 8:
+    elif len(userPassword) < 8: #checks whether the password length is not less than 8 characters.
         print("The password should be atleast 8 characters long.")
     else:
         print("Correct format of password.")
         break
         
-    counter -= 1
-    if counter == 0:
+    counter -= 1 #decreases counter by 1.
+    if counter == 0: #iF counter is equal to 0, then it means user has used all the attempts leFt.
         print("You have used 3 attempts which is the maximum number of attempts allowed. Please try again later.")
